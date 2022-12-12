268620ce43e70cbfdcb020daa5ba0a000a564f15
spaubleit <spaubleit@gmail.com>
2022-12-12 11:11:50 +0300

    initial commit

---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  description = "NixOS configuration";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-22.05";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    devenv.url = "github:cachix/devenv/v0.4";
+    home-manager.url = "github:nix-community/home-manager/release-22.05";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+  outputs = inputs@{ nixpkgs, home-manager, devenv, ... }: 
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+      };
+      pkgs-unstable = import nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+      };
+    in {
+      nixosConfigurations = {
+        desktop = nixpkgs.lib.nixosSystem {
+          inherit system;
+          modules = [
+            ./configuration.nix
+            home-manager.nixosModules.home-manager {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.users.spaubleit = import ./home.nix { 
+                config = {};
+                inherit pkgs pkgs-unstable system devenv; 
+              };
+            }
+          ];
+        };
+      };
+    };
+}
---
+++ home.nix
@@ -0,0 +1,65 @@
+{ config, pkgs, pkgs-unstable, devenv, system, ... }:
+{
+  home = {
+    username = "spaubleit";
+    homeDirectory = "/home/spaubleit";
+    stateVersion = "22.05";
+    
+    packages = with pkgs; [
+      # Utils
+      cloc
+      nodejs
+      yarn
+      git
+    
+      # Apps
+      firefox
+      google-chrome
+      pkgs-unstable.jetbrains.webstorm
+      mozillavpn
+      obsidian
+      libreoffice
+      spotify
+      protonvpn-gui
+      transmission-gtk
+      
+      # Messengers
+      tdesktop
+      teams
+      slack
+      zoom-us
+      skypeforlinux
+      
+      # Graphics
+      krita
+      gimp
+      
+      # gnome
+      gnome.gnome-tweaks
+      gnomeExtensions.syncthing-icon
+      gnomeExtensions.gtk-title-bar
+      gnomeExtensions.tray-icons-reloaded
+      gnomeExtensions.pop-shell
+      
+      devenv.packages.x86_64-linux.devenv
+    ];
+  };
+  
+  services.syncthing = {
+    enable = true;
+  };
+  
+  programs = {
+    bash.enable = true;
+    direnv = {
+      enable = true;
+      enableBashIntegration = true;
+    };
+    starship = {
+      enable = true;
+      enableBashIntegration = true;
+      settings = {};
+    };
+    home-manager.enable = true;
+  };
+}
